2f6535e5adf2f75fe47f2871bb183d92dfb8e5e6
Pepesob <113636251+Pepesob@users.noreply.github.com>
2024-04-21 22:25:48 +0200

    Deleted console.log

--- src/services/classes.service.ts
+++ src/services/classes.service.ts
@@ -55,7 +55,6 @@ export const addClass = async (values: AddClassFormType) => {
 			description: values.subjectDescription,
 			tagNames: values.subjectTags,
 		};
-		console.log(Endpoints.SUBJECTS);
 		return await fetcher<Course>(Endpoints.SUBJECTS, {
 			method: 'POST',
 			body: toSend,
